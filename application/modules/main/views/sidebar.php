174d9ea901ad6840750c47db58baeb5abf905ffe
Hevin Ebarle <104123469+H3vin28@users.noreply.github.com>
2025-05-17 11:06:00 +0800

    Add files via upload

--- application/modules/main/views/sidebar.php
+++ application/modules/main/views/sidebar.php
@@ -1,19 +1,25 @@
 <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: rgba(5, 19, 67, 1);">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link" style="background-color: rgba(5, 19, 67, 1);">
+    <!-- <a href="#" class="brand-link" style="background-color: rgba(5, 19, 67, 1);">
       <img src="<?= base_url('assets/images/HEMC1.png')?>" alt="AdminLTE Logo" class="" style="width: 100%;">
-    </a>
+    </a> -->
 
     <!-- Sidebar -->
-    <div class="sidebar" style="margin-top: 125px;">
+    <div class="sidebar">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="<?= base_url('assets/images/HEMC1.png')?>" alt="AdminLTE Logo" class="" style="width: 96%;">
+        </div>
+      </div>
+
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="<?= base_url('assets/images/pro_pic.png')?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block" <?= @$row != null ? '':'data-toggle="modal" data-target="#login-modal"'?> ><?= @$row != null ? '@'.$row->username:'Login'?></a>
+          <a href="#" class="d-block" data-toggle="modal" <?= @$row != null ? 'data-target="#user-modal"':' data-target="#login-modal"'?> ><?= @$row != null ? '@'.$row->username:'Login'?></a>
         </div>
       </div>
 
@@ -57,7 +63,7 @@
           <div class="modal-body">
             <div class="row">
               <div class="col-sm-12">
-                <label>Email Address</label>
+                <label>Email Address or Username</label>
                 <div class="input-group mb-3">
                   <input type="text" class="form-control" name="email" placeholder="Enter Username or Email">
                   <div class="input-group-append">
@@ -85,5 +91,42 @@
           </div>
         </form> 
       </div> 
+    </div>
+  </div>
+  <div class="modal fade" id="user-modal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">USER INFORMATION</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-group">
+            <label>Full Name:</label>
+            <p class="form-control-plaintext"><?= $row->fullname ?></p>
+          </div>
+          
+          <div class="form-group">
+            <label>Username:</label>
+            <p class="form-control-plaintext"><?= $row->username ?></p>
+          </div>
+
+          <div class="form-group">
+            <label>Email Address:</label>
+            <p class="form-control-plaintext"><?= $row->email ?></p>
+          </div>
+
+          <div class="form-group">
+            <label>Password:</label>
+            <input type="password" class="form-control" value="********" readonly>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary"><i class="fas fa-edit"></i> EDIT INFORMATION</button>
+        </div>
+      </div>
     </div>
   </div>
